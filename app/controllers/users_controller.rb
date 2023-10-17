08f7b4a3a74bce90d6fdfd04d182536b9174856f
EC2 Default User <piyopiyoblue5@gmail.com>
2023-10-17 10:09:25 +0000

    [Save]4-27-34

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   
   
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "homes/about" => "homes#about", as: "about"
   resources :books
   resources :users, only: [:index, :show, :edit, :update]
+  post 'books' => 'books#create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
