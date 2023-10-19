a5b4abd372fb167b7e79cd9e8c700a30dd5a07d4
EC2 Default User <piyopiyoblue5@gmail.com>
2023-10-19 05:07:56 +0000

    [Save]4-27-46

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,21 @@ class UsersController < ApplicationController
   end
 
   def index
-    @user = User.new
+    @book = Book.new
+    @user = current_user
     @users = User.all
   end
 
   def show
+    @book = Book.new
     @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
   end
+  
 
   def update
     @user = User.find(params[:id])
