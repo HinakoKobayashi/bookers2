e01322319278858cb18eb71589d5b6986638ec91
EC2 Default User <piyopiyoblue5@gmail.com>
2023-10-21 11:57:58 +0000

    [Save]4-27-64

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,12 +17,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    user = User.find(params[:id])
-   unless user.id == current_user.id
-    redirect_to user_path(@user)
-   end
-
     @user = User.find(params[:id])
+   unless @user.id == current_user.id
+    redirect_to user_path(current_user.id)
+   end
   end
 
 
@@ -54,7 +52,7 @@ class UsersController < ApplicationController
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-      @user.image
+      @user.profile_image
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, name_length: 2..20
+         :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
 
   has_one_attached :image
 
-  validates :name, presence: true, length: { minimum:2 maximum: 20 }
+  validates :name, presence: true, length: { minimum:2, maximum: 20 }
   validates :introduction, presence: true, length: { maximum: 50 }
 
   def get_profile_image(width, height)
